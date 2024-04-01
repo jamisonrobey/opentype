3d27e8f0ff93e79092560bf787ee897133e0e0e7
jamison <jamisonrobey@gmail.com>
2024-04-01 19:08:54 +1000

    Styling: way cleaner fading of div

--- components/TypingTest/TypingModeSelector/TypingModeSelector.tsx
+++ components/TypingTest/TypingModeSelector/TypingModeSelector.tsx
@@ -18,7 +18,6 @@ export const TypingModeSelector = () => {
   const handleWordsTimeQuoteChange = (selected: WordsTimeQuote) => {
     setSelectedWordsTimeQuote(selected);
     setSelectedLength(getDefaultLengthForMode(selected));
-
     if (selected === "quote") {
       setShowPunctuationNumbers(false);
       setSelectedPunctuationNumbers([]);
@@ -30,30 +29,33 @@ export const TypingModeSelector = () => {
   return (
     <div className="w-full flex justify-center">
       <div
-        className={`flex p-4 sm:px-1 text-xs transition-all duration-75 items-center justify-evenly w-8/10 2xl:w-3/7 rounded-lg bg-[var(--bgDark-color)]`}
+        className={`flex p-4 sm:px-4 text-xs items-center justify-evenly w-8/10 2xl:w-3/7 rounded-lg bg-[var(--bgDark-color)]`}
       >
         <div
           className={`${
-            showPunctuationNumbers ? "flex-grow" : "flex-grow-0 w-0 opacity-0"
-          } transition-all duration-300 ease-linear transition-width `}
+            showPunctuationNumbers
+              ? "flex-grow opacity-100 transition-opacity duration-200 ease-linear"
+              : "flex-grow-0 opacity-0 transition-opacity duration-200 ease-linear"
+          } ${
+            showPunctuationNumbers
+              ? "w-auto transition-width duration-300 ease-linear delay-200"
+              : "w-0 transition-width duration-300 ease-linear"
+          }`}
         >
           <PunctuationNumbersSelector />
         </div>
         <div
           className={`${
-            showPunctuationNumbers ? "" : "opacity-0"
-          }w-[3px] mx-4 bg-[var(--darkAccent-color)] h-2/3`}
+            showPunctuationNumbers
+              ? "w-[3px] mx-4 bg-[var(--darkAccent-color)] h-2/3 transition-all duration-75 ease-linear"
+              : "w-0 mx-0 bg-[var(--darkAccent-color)] h-2/3 transition-all duration-75 ease-linear"
+          }`}
         ></div>
-        <div
-          className={`${
-            showPunctuationNumbers ? "w-[3]px]" : "w-0"
-          } bg-[var(--darkAccent-color)] h-2/3 duration-300 transition-all ease-linear transition-width delay-200`}
-        ></div>
-        <div className="flex-grow transition-spacing duration-300 ease-linear">
+        <div className="flex-grow">
           <WordsTimeQuoteSelector onChange={handleWordsTimeQuoteChange} />
         </div>
         <div className="w-[3px] mx-4 bg-[var(--darkAccent-color)] h-2/3"></div>
-        <div className="flex-grow transition-all duration-300 ease-linear">
+        <div className="flex-grow">
           <LengthSelector />
         </div>
       </div>
