d887deadc54db33506a13c6ed4315697b0f39ea9
jamison <jamisonrobey@gmail.com>
2024-04-02 11:37:40 +1000

    Feature: Decently accurate gameplay feel to monkeytype, cursor animations, text scrolling and char coloring.

--- components/Game/Game.tsx
+++ components/Game/Game.tsx
@@ -1,65 +1,91 @@
+import { useRef, useEffect, useState } from "react";
 import { useGameContext } from "./GameContext";
-import { useEffect, useRef } from "react";
 
 export const Game: React.FC = () => {
   const { words, userInput, handleInputChange, typedWords } = useGameContext();
-  const wordsRef = useRef<(HTMLSpanElement | null)[]>([]);
+  const cursorRef = useRef<HTMLSpanElement>(null);
+  const [cursorPosition, setCursorPosition] = useState({ left: 0, top: 0 });
 
   useEffect(() => {
     const currentWordIndex = typedWords.length;
-    const currentWordElement = wordsRef.current[currentWordIndex];
+    const currentWordEl = document.getElementById(`word-${currentWordIndex}`);
+    const currentCharIndex = userInput.length;
+    const currentWord = words[currentWordIndex];
 
-    if (currentWordElement) {
-      currentWordElement.scrollIntoView({
+    if (currentWordEl) {
+      currentWordEl.scrollIntoView({
         behavior: "smooth",
         block: "center",
       });
     }
-  }, [userInput, typedWords]);
 
-  const getCharClass = (wordIndex: number, charIndex: number) => {
-    if (wordIndex < typedWords.length) {
-      const typedWord = typedWords[wordIndex];
-      if (charIndex < typedWord.length) {
-        return typedWord[charIndex] === words[wordIndex][charIndex]
-          ? "text-green-500"
-          : "text-red-500";
-      }
-    } else if (wordIndex === typedWords.length) {
-      const currentWord = words[wordIndex];
-      if (charIndex < userInput.length) {
-        return userInput[charIndex] === currentWord[charIndex]
-          ? "text-green-500"
-          : "text-red-500";
-      } else if (charIndex === userInput.length) {
-        return "text-yellow-500";
+    const charEl = currentWordEl?.children[
+      currentCharIndex
+    ] as HTMLElement | null;
+
+    if (charEl) {
+      const newPosition = {
+        left: charEl.offsetLeft,
+        top: charEl.offsetTop,
+      };
+      setCursorPosition(newPosition);
+    } else if (currentCharIndex === currentWord.length) {
+      const lastCharEl = currentWordEl?.lastChild as HTMLElement | null;
+      if (lastCharEl) {
+        const newPosition = {
+          left: lastCharEl.offsetLeft + lastCharEl.offsetWidth,
+          top: lastCharEl.offsetTop,
+        };
+        setCursorPosition(newPosition);
       }
     }
-    return "";
-  };
+  }, [userInput, typedWords, words]);
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      cursorRef.current?.style.setProperty("left", `${cursorPosition.left}px`);
+      cursorRef.current?.style.setProperty("top", `${cursorPosition.top}px`);
+    });
+  }, [cursorPosition]);
 
   return (
     <div className="w-3/4">
-      <div className="text-3xl mb-4 h-28  overflow-auto no-scrollbar">
+      <div className="text-3xl mb-4 h-28 overflow-auto no-scrollbar relative">
         {words.map((word, wordIndex) => (
           <span
             key={wordIndex}
-            className="inline-block mr-6"
-            ref={(el) => {
-              wordsRef.current[wordIndex] = el;
-              return undefined;
-            }}
+            id={`word-${wordIndex}`}
+            className={`inline-block mr-6 ${
+              wordIndex < typedWords.length && word !== typedWords[wordIndex]
+                ? "border-b border-b-red-500"
+                : ""
+            }`}
           >
             {word.split("").map((char, charIndex) => (
               <span
                 key={charIndex}
-                className={getCharClass(wordIndex, charIndex)}
+                className={
+                  wordIndex < typedWords.length
+                    ? typedWords[wordIndex][charIndex] === char
+                      ? "text-[var(--title-color)]"
+                      : "text-red-500"
+                    : wordIndex === typedWords.length &&
+                      charIndex < userInput.length
+                    ? userInput[charIndex] === char
+                      ? "text-[var(--title-color)]"
+                      : "text-red-500"
+                    : "text-[var(--text-color)]"
+                }
               >
                 {char}
               </span>
             ))}
           </span>
         ))}
+        <span
+          ref={cursorRef}
+          className="border-r border-r-[var(--accent-color)] h-[1.5em] absolute transition-all ease-linear duration-[0.05s]"
+        />
       </div>
       <input
         type="text"
